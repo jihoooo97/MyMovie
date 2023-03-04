d792ae70cd18bcb2708801bf84b12a673f55554e
Jiho <yjh7827@naver.com>
2023-03-04 23:15:06 +0900

    init

---
+++ Common/Common/Common.h
@@ -0,0 +1,18 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by 유지호 on 2023/03/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
---
+++ Home/Home/Home.h
@@ -0,0 +1,18 @@
+//
+//  Home.h
+//  Home
+//
+//  Created by 유지호 on 2023/03/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Home.
+FOUNDATION_EXPORT double HomeVersionNumber;
+
+//! Project version string for Home.
+FOUNDATION_EXPORT const unsigned char HomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Home/PublicHeader.h>
+
+
