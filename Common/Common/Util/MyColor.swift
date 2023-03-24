fb05bbc631347b53633f01ffbaffbfc085c0dc58
Jiho <yjh7827@naver.com>
2023-03-25 00:42:20 +0900

    Color Asset 추가

---
+++ Common/Common/Util/MyColor.swift
@@ -0,0 +1,45 @@
+//
+//  MyColor.swift
+//  Common
+//
+//  Created by 유지호 on 2023/03/24.
+//
+
+import UIKit
+
+public enum MyColor {
+    
+    public static let G50 = UIColor(named: "G50")!
+    public static let G100 = UIColor(named: "G100")!
+    public static let G200 = UIColor(named: "G200")!
+    public static let G300 = UIColor(named: "G300")!
+    public static let G400 = UIColor(named: "G400")!
+    public static let G500 = UIColor(named: "G500")!
+    public static let G600 = UIColor(named: "G600")!
+    public static let G700 = UIColor(named: "G700")!
+    public static let G800 = UIColor(named: "G800")!
+    public static let G900 = UIColor(named: "G900")!
+
+    public static let P50 = UIColor(named: "P50")!
+    public static let P100 = UIColor(named: "P100")!
+    public static let P200 = UIColor(named: "P200")!
+    public static let P300 = UIColor(named: "P300")!
+    public static let P400 = UIColor(named: "P400")!
+    public static let P500 = UIColor(named: "P500")!
+    public static let P600 = UIColor(named: "P600")!
+    public static let P700 = UIColor(named: "P700")!
+    public static let P800 = UIColor(named: "P800")!
+    public static let P900 = UIColor(named: "P900")!
+
+    public static let S50 = UIColor(named: "S50")!
+    public static let S100 = UIColor(named: "S100")!
+    public static let S200 = UIColor(named: "S200")!
+    public static let S300 = UIColor(named: "S300")!
+    public static let S400 = UIColor(named: "S400")!
+    public static let S500 = UIColor(named: "S500")!
+    public static let S600 = UIColor(named: "S600")!
+    public static let S700 = UIColor(named: "S700")!
+    public static let S800 = UIColor(named: "S800")!
+    public static let S900 = UIColor(named: "S900")!
+    
+}
